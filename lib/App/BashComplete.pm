5aab213ee4a80ef70ca2aa21c5d13b9b316ae9b5
Pedro Melo <melo@simplicidade.org>
2010-06-14 13:20:56 +0100

    Added command line parsing, updated docs
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/App/BashComplete.pm
+++ lib/App/BashComplete.pm
@@ -5,11 +5,71 @@ package App::BashComplete;
 use strict;
 use warnings;
 use Bash::Completion;
+use Getopt::Long qw(GetOptionsFromArray);
 
-sub new { return bless {}, shift }
+############
+# Attributes
 
+=attr opts
+
+Returns an HashRef with all the command line options used.
+
+=cut
+sub opts { return $_[0]->{opts} }
+
+=attr cmd_line
+
+Returns a ArrayRef with the parts of the command line that could not be parsed as options.
+
+=cut
+sub cmd_line { return $_[0]->{cmd_line} }
+
+
+#########
+# Methods
+
+=method new
+
+Creates a new empty instance.
+
+=cut
+sub new { return bless {opts => {}, cmd_line => []}, shift }
+
+
+=method run
+
+Processes options, using both command line and arguments to run(), and
+executes the proper action.
+
+=cut
 sub run {
-  
+  my $self = shift;
+
+  return unless $self->_parse_options(@_);
+
+  # TODO: deal with opts
+}
+
+
+#######
+# Utils
+
+sub _parse_options {
+  my $self = shift;
+
+  my $cmd_line = $self->{cmd_line} = [@_, @ARGV];
+  my $opts = $self->{opts} = {};
+
+  my $ok = GetOptionsFromArray($cmd_line, $opts, 'setup');
+
+  # TODO: deal with !$ok
+
+  return $ok;
 }
 
 1;
+
+
+__END__
+
+
--- t/50-app-bash-complete.t
+++ t/50-app-bash-complete.t
@@ -5,10 +5,14 @@ use warnings;
 use Test::More;
 
 use_ok('App::BashComplete') || die "Could not load App::BashComplete, ";
+local @ARGV = ();
 
 my $app = App::BashComplete->new;
 ok($app, 'Have a nice app');
 can_ok($app, qw( run ));
+
+ok($app->_parse_options('--setup'), "Parsed '--setup' cmd line");
+ok($app->opts->{setup}, '... setup option properly set');
 
 
 ## and we are done for today
