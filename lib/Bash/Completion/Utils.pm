ec6afb4fe3094aabc0d23538a71c6a7a1fad62bc
Pedro Melo <melo@simplicidade.org>
2010-06-14 15:27:26 +0100

    Skel for Bash::Completion::Utils
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ lib/Bash/Completion/Utils.pm
@@ -0,0 +1,23 @@
+package Bash::Completion::Utils;
+
+use strict;
+use warnings;
+use parent 'Exporter';
+use File::Spec::Functions;
+
+@Bash::Completion::Utils::EXPORT_OK = qw(
+);
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    use Bash::Completion::Utils qw( );
+    
+    ...
+
+=head1 DESCRIPTION
+
+A library of utilities functions usefull to plugin writers.
