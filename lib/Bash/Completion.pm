547ce4fbea82969229f4306ddaad386e50eeffdd
Pedro Melo <melo@simplicidade.org>
2010-06-19 16:23:28 +0100

    Added Bash::Completion new() and load_plugins()
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Bash/Completion.pm
+++ lib/Bash/Completion.pm
@@ -4,7 +4,32 @@ package Bash::Completion;
 
 use strict;
 use warnings;
+use Module::Pluggable sub_name => 'list_plugins';
 
+=method new
+
+Create a L<Bash::Completion> instance.
+
+=cut
+
+sub new { return bless {}, $_[0] }
+
+
+=method load_plugins
+
+Search C<@INC> for all classes in the L<Bash::Completion::Plugin> namespace.
+
+=cut
+
+sub load_plugins {
+  my ($self) = @_;
+
+  unless ($self->{plugins}) {
+    $self->{plugins} = [map { $_->new } $self->list_plugins];
+  }
+
+  return @{$self->{plugins}};
+}
 
 
 1;
--- t/10-bash-completion.t
+++ t/10-bash-completion.t
@@ -6,6 +6,15 @@ use Test::More;
 
 use_ok('Bash::Completion') || die "Could not load Bash::Completion, ";
 
+my $bc = Bash::Completion->new;
+ok($bc,                  'Bash::Completion object ok');
+ok(scalar($bc->plugins), '... got some plugins');
+
+my @plugins = $bc->load_plugins;
+for my $plugin (@plugins) {
+  ok($plugin->isa('Bash::Completion::Plugin'), " ... plugin $plugin is ok");
+}
+
 
 ## and we are done for today
 done_testing();
