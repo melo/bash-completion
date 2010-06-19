6f1c46c4320b7d6d5cc0fc9c3eab5163bcca9868
Pedro Melo <melo@simplicidade.org>
2010-06-19 13:34:32 +0100

    Added a completion request object
    Hide all the complexity of parsing COMP_* environment variables.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ lib/Bash/Completion/Request.pm
@@ -0,0 +1,33 @@
+package Bash::Completion::Request;
+
+use strict;
+use warnings;
+
+sub new {
+  my ($class) = @_;
+
+  return bless {
+    line  => $ENV{COMP_LINE},
+    point => $ENV{COMP_POINT},
+    word  => _get_completion_word(),
+  }, $class;
+}
+
+## Stolen from http://github.com/yanick/dist-zilla/blob/master/contrib/dzil-complete
+sub _get_completion_word {
+  my $comp = substr $ENV{'COMP_LINE'}, 0, $ENV{'COMP_POINT'};
+  $comp =~ s/.*\h//;
+  return $comp;
+}
+
+sub line { return $_[0]{line} }
+sub word { return $_[0]{word} }
+
+sub candidate {
+  my $self = shift;
+
+  print "$_\n" for @_;
+}
+
+
+1;
---
+++ t/03-request.t
@@ -0,0 +1,18 @@
+#!perl
+
+use strict;
+use warnings;
+use Test::More;
+
+use_ok('Bash::Completion::Request')
+  or die "Could not load 'Bash::Completion::Request', ";
+
+$ENV{COMP_LINE}  = 'abcd efgh word';
+$ENV{COMP_POINT} = 12;
+my $r = Bash::Completion::Request->new;
+
+ok($r, 'Created Request instance ok');
+is($r->line, 'abcd efgh word', '... expected command line');
+is($r->word, 'wo',             '... expected parsed word');
+
+done_testing();
